71849786f161e4a95c5feba54941e61ecd7b6609
miniengineer <samadova.minira@gmail.com>
2021-09-28 22:01:11 +0400

    set up config for static and api request handling, add test React component, refactor hello_react to App component, add react-router

---
+++ app/javascript/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Posts from '../components/Posts';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Switch>
+        <Route exact path="/" component={Posts} />
+      </Switch>
+    </React.Fragment>
+  )
+}
+
+export default App;
---
+++ app/javascript/src/components/Posts.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Posts = () => <div>I am posts component</div>;
+
+export default Posts;
