0a29ce9a9ca7c085a4eee299a57546577fdf1237
miniengineer <samadova.minira@gmail.com>
2021-10-17 16:46:53 +0900

    configure devise to use custom controllers for sign up and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
 
-  namespace :api do
+  devise_for :users
+
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :posts
+
+      devise_scope :user do
+        post 'sign_up', to: 'registrations#create'
+        post 'sign_in', to: 'sessions#create'
+      end
     end
   end
-
-  devise_for :users
 
   get '*path', to: 'pages#index', via: :all
 end
