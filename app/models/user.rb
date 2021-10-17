ddac22c597e6bc02322a0a331e86c61266cd3f5c
miniengineer <samadova.minira@gmail.com>
2021-10-17 16:43:36 +0900

    add comments to User model about using validates :email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+
+  # it's either :validatable from device or validates :email, uniqueness: true
+  # having both of them causes two validations and two error messages in ActiveModel::Errors
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
