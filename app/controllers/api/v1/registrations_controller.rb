1d45e7d0170b8fca9a13ab685dfa3621ad6b3249
miniengineer <samadova.minira@gmail.com>
2021-10-20 08:41:46 +0900

    remove comments about ActiveModel::Errors + use .save and return model errors instead of handling exception

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -5,38 +5,14 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   def create
     user = User.new user_params
 
-    # error message wasn't showing up because I was using .save instead of .save!
-    # also apparently you can't use rescue without begin
-    begin user.save!
+    if user.save
       render json: {
         data: { user_id: user.id },
         messages: ['User created successfully']
       }, status: :ok
-
-    # apparently to catch an error related to DB it's necessary to use RecordInvalid
-    # Raised by ActiveRecord::Base#save! and ActiveRecord::Base#create! (from the Rails api doc)
-    # https://api.rubyonrails.org/v6.1.0/classes/ActiveRecord/RecordInvalid.html
-
-
-    # this is what you get if you're just using ActiveRecord::RecordInvalid => e
-    # pp e
-    # => <ActiveRecord::RecordInvalid: Validation failed: Email has already been taken, Email has already been taken>
-
-    # and this is what you get if you're using the proper invalid
-    # pp invalid.record.errors
-    # => 
-    #<ActiveModel::Errors:0x00007ff00d287468
-    # @base=
-    # #<User id: nil, email: "test@mail.com", created_at: nil, updated_at: nil>,
-    # @details=
-    # {:email=>
-    #   [{:error=>:taken, :value=>"test@mail.com"},
-    #     {:error=>:taken, :value=>"test@mail.com"}]},
-    # @messages={:email=>["has already been taken", "has already been taken"]}>
-
-    rescue ActiveRecord::RecordInvalid => invalid
+    else
       render json: {
-        messages: invalid.record.errors.full_messages
+        messages: user.errors.full_messages
       }, status: :bad_request
     end
   end
