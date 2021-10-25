8d72c12431e5dc17f565d8cbe5cc312a6af68501
miniengineer <samadova.minira@gmail.com>
2021-10-25 18:35:09 +0900

    do not overwrite Devise methods, add respond_to json

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,33 +1,4 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
-  before_action :ensure_params_exist, only: :create
+  respond_to :json
   skip_before_action :verify_authenticity_token, :only => :create
-
-  def create
-    user = User.new user_params
-
-    if user.save
-      render json: {
-        data: { user_id: user.id },
-        messages: ['User created successfully']
-      }, status: :ok
-    else
-      render json: {
-        messages: user.errors.full_messages
-      }, status: :bad_request
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :password)
-  end
-
-  def ensure_params_exist
-    return if params[:user].present?
-
-    render json: {
-      messages: ['Missing params necessary for user registration']
-    }, status: :bad_request
-  end
 end
