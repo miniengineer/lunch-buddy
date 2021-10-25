55e81c559c5d5dfa1a01d4710beace3649d1e357
miniengineer <samadova.minira@gmail.com>
2021-10-25 18:43:57 +0900

    set up devise to use custom sessions and registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  devise_for :users, path: 'api/v1/users', controllers: { sessions: 'api/v1/sessions', registrations: 'api/v1/registrations' }
 
-  devise_for :users
-
-  namespace :api, defaults: { format: :json } do
+  namespace :api do
     namespace :v1 do
       resources :posts
-
-      devise_scope :user do
-        post 'sign_up', to: 'registrations#create'
-        post 'sign_in', to: 'sessions#create'
-      end
     end
   end
+  root 'pages#index'
+
 
   get '*path', to: 'pages#index', via: :all
 end
