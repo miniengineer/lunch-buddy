285bf29366dd2e7e7851ef1186144183d6434650
miniengineer <samadova.minira@gmail.com>
2021-12-14 17:30:27 +0900

    fix bug: incorrect path

--- app/javascript/src/components/Home.js
+++ app/javascript/src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router';
 
 const Home = () => {
-  return <Redirect to="/signin" />;
+  return <Redirect to="/sign_in" />;
 };
 
 export default Home;
