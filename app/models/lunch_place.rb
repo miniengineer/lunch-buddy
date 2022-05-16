6297333859cfe8e905453fce433ffba2f0517d99
miniengineer <samadova.minira@gmail.com>
2022-05-16 17:31:46 +0900

    replace uniqueness constrant with presence for google_places_id column

--- app/models/lunch_place.rb
+++ app/models/lunch_place.rb
@@ -3,6 +3,6 @@
 class LunchPlace < ApplicationRecord
   belongs_to :user
 
-  validates :user_id, presence: true
-  validates :google_places_id, uniqueness: true
+  validates :user_id, presence: true, index: true
+  validates :google_places_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_011400) do
+ActiveRecord::Schema.define(version: 2022_02_16_231543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2022_02_17_011400) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["google_places_id"], name: "index_lunch_places_on_google_places_id", unique: true
     t.index ["user_id"], name: "index_lunch_places_on_user_id"
   end
 
