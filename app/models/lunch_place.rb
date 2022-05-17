39e9766dd2539546eb303019deb21b98d0f14a07
miniengineer <samadova.minira@gmail.com>
2022-05-17 10:24:37 +0900

    do it rubocop way

--- app/models/lunch_place.rb
+++ app/models/lunch_place.rb
@@ -4,6 +4,5 @@ class LunchPlace < ApplicationRecord
   belongs_to :user
 
   validates :user_id, presence: true
-  validates :google_places_id, uniqueness: true
-  validates_uniqueness_of :google_places_id, scope: :user
+  validates :google_places_id, uniqueness: { scope: :user }
 end
